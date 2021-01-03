1ab73985ddb6176eed757033d4fcd3a6e4483312
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-01-03 04:57:44 +0200

    Update example 3 to use metalog 3.0

--- JavaScript/3-sandbox/main.js
+++ JavaScript/3-sandbox/main.js
@@ -1,39 +1,35 @@
 'use strict';
 
 const vm = require('vm');
-const fs = require('fs');
+const fsp = require('fs').promises;
 const util = require('util');
 const metalog = require('metalog');
 
-const log = metalog({
-  path: './log',
-  node: 'S1N1',
-  writeInterval: 5000,
-  writeBuffer: 64 * 1024,
-  keepDays: 5,
-  toStdout: [],
-}).bind('app1');
+(async () => {
 
-const logInfo = (...args) => log.info(util.format(...args));
-const logError = (...args) => log.error(util.format(...args));
+  const logger = await metalog.openLog({
+    path: './log',
+    workerId: 1,
+    writeInterval: 5000,
+    writeBuffer: 64 * 1024,
+    keepDays: 5,
+    toStdout: [],
+    home: process.cwd(),
+  });
 
-const context = {
-  module: {},
-  console: {
-    log: logInfo,
-    dir: logInfo,
-    debug: logInfo,
-    error: logError,
-  },
-};
+  const context = {
+    module: {},
+    console: logger.console,
+  };
 
-context.global = context;
-const sandbox = vm.createContext(context);
+  context.global = context;
+  const sandbox = vm.createContext(context);
 
-const fileName = './application.js';
-fs.readFile(fileName, 'utf8', (err, src) => {
+  const fileName = './application.js';
+  const src = await fsp.readFile(fileName, 'utf8');
   const script = new vm.Script(`module.exports = () => {\n${src}\n};`);
   const execute = script.runInNewContext(sandbox);
   execute();
-  log.logger.close();
-});
+  logger.close();
+
+})();
