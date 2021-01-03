d620cba200fbd3e0500ba61c48592ea95b54e3fa
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-01-03 04:50:55 +0200

    Refactor example 2 to use metalog 3.0

--- JavaScript/2-metalog/main.js
+++ JavaScript/2-metalog/main.js
@@ -2,40 +2,52 @@
 
 const metalog = require('metalog');
 
-const log = metalog({
-  path: './log',
-  node: 'S1N1',
-  writeInterval: 3000,
-  writeBuffer: 64 * 1024,
-  keepDays: 5,
-  toStdout: [],
-}).bind('app1');
+(async () => {
 
-log.logger.on('error', err => {
-  console.log('Error');
-  console.error(err);
-  process.exit(1);
-});
-
-log.logger.on('open', () => {
-  console.log('Opened');
-  log.system('System test log message');
-  log.fatal('Fatal test log message');
-  log.error('Error test log message');
-  log.warn('Warning test log message');
-  log.info('Info test log message');
-  log.debug('Debug test log message');
-  log.slow('Slow test log message');
-  log.db('Database test log message');
-
-  const begin = process.hrtime.bigint();
-  for (let i = 0; i < 1000000; i++) {
-    log.info('Write more then 60Mb logs, line: ' + i);
-  }
-  log.logger.close();
-  log.logger.on('close', () => {
-    const end = process.hrtime.bigint();
-    const time = (end - begin) / 1000000n;
-    console.log(`Time: ${time} milliseconds`);
+  const logger = await metalog.openLog({
+    path: './log',
+    workerId: 1,
+    writeInterval: 3000,
+    writeBuffer: 64 * 1024,
+    keepDays: 5,
+    toStdout: ['log', 'info', 'warn', 'error'],
+    home: process.cwd(),
   });
-});
+
+  logger.console.assert(true, 'Assert message: passed');
+  logger.console.assert(false, 'Assert message: not passed');
+  logger.console.count('count-label');
+  logger.console.countReset('count-label');
+  logger.console.debug('Test log message for console.debug', 'arg2');
+  logger.console.dir('Test log message for console.dir', 'arg2');
+  const err = new Error('Test log message for console.error', 'arg2');
+  logger.console.error(err);
+  logger.console.group('Test log message for console.group', 'arg2');
+  logger.console.groupCollapsed('Test log message for console.group', 'arg2');
+  logger.console.groupEnd();
+  logger.console.info('Test log message for console.info', 'arg2');
+  logger.console.log('Test log message for console.log', 'arg2');
+  logger.console.table([
+    { a: 1, b: 2 },
+    { a: 3, b: 4 },
+  ]);
+  logger.console.time('time-label');
+  logger.console.timeEnd('time-label');
+  logger.console.timeLog('time-label', 'Test log message for console.timeLog');
+  logger.console.trace('Test log message for console.trace', 'arg2');
+  logger.console.warn('Test log message for console.warn', 'arg2');
+
+  const begin = process.hrtime();
+  for (let i = 0; i < 1000000; i++) {
+    logger.console.debug('Write more then 60Mb logs, line: ' + i);
+  }
+
+  logger.on('close', () => {
+    const end = process.hrtime(begin);
+    const time = end[0] * 1e9 + end[1];
+    console.log({ time });
+  });
+
+  await logger.close();
+
+})();
